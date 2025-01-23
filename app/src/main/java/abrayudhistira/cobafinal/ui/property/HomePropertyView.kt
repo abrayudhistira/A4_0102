15ee544be55dca8fa368074f4f83f17fee341d38
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-01-23 08:58:48 +0700

    Deklarasi route untuk home view

---
+++ app/src/main/java/abrayudhistira/cobafinal/ui/property/HomePropertyView.kt
@@ -0,0 +1,17 @@
+package abrayudhistira.cobafinal.ui.property
+
+import abrayudhistira.cobafinal.model.Properti
+import abrayudhistira.cobafinal.ui.navigasi.DestinasiNavigasi
+import android.content.Context
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Property"
+}
+
+class PropertiAdapter() {
+}
