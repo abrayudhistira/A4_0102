3b42912003dd80dc2bb8d529727ee05946c24cae
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-01-24 03:36:35 +0700

    Membuat Service & Repository ManajerProperty

---
+++ app/src/main/java/abrayudhistira/cobafinal/repository/ManajerPropertyRepository.kt
@@ -0,0 +1,44 @@
+package abrayudhistira.cobafinal.repository
+
+import abrayudhistira.cobafinal.model.ManajerProperti
+import abrayudhistira.cobafinal.service.ManajerPropertiService
+
+
+interface ManajerPropertyRepository {
+    suspend fun getManajerProperti() : List<ManajerProperti>
+    suspend fun getbyidManajerProperti(idManajerProperti : String): ManajerProperti
+    suspend fun insertManajerProperti(idManajerProperti: ManajerProperti)
+    suspend fun editManajerProperti(idManajerProperti: String,manajerProperti: ManajerProperti)
+    suspend fun deleteManajerProperti(idManajerProperti: String)
+}
+class NetworkManajerPropertyRepository(private val manajerPropertiService: ManajerPropertiService)
+    : ManajerPropertyRepository {
+    override suspend fun getManajerProperti(): List<ManajerProperti> = manajerPropertiService.getManajerProperti()
+    override suspend fun getbyidManajerProperti(idManajerProperti: String): ManajerProperti {
+        return manajerPropertiService.getbyidManajerProperti(idManajerProperti)
+    }
+
+    override suspend fun insertManajerProperti(manajerProperti: ManajerProperti) {
+        manajerPropertiService.insertManajerProperti(manajerProperti)
+    }
+
+    override suspend fun editManajerProperti(idManajerProperti: String, manajerProperti: ManajerProperti) {
+        manajerPropertiService.editManajerProperti(idManajerProperti, manajerProperti)
+    }
+
+    override suspend fun deleteManajerProperti(idManajerProperti: String) {
+        try {
+            val response = manajerPropertiService.deleteManajerProperti(idManajerProperti)
+            if (!response.isSuccessful) {
+                throw Exception("Failed to delete jenis properti. HTTP Status Code: ${response.code()}")
+            }
+            else{
+                response.message()
+                println(response.message())
+            }
+        } catch (e: Exception) {
+            throw e
+        }
+    }
+
+}
--- app/src/main/java/abrayudhistira/cobafinal/service/ManajerPropertiService.kt
+++ app/src/main/java/abrayudhistira/cobafinal/service/ManajerPropertiService.kt
@@ -10,7 +10,7 @@ import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
 
-interface ManajerProperty {
+interface ManajerPropertiService {
     @Headers(
         "Accept: application/json",
         "Content-Type: application/json",
