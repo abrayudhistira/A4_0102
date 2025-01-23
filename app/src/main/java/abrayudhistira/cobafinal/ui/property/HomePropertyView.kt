1ce31395cfb850c689a28fc04010fa11ac98d5fc
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-01-23 09:10:01 +0700

    Membuat fungsi OnLoading dan OnError

--- app/src/main/java/abrayudhistira/cobafinal/ui/property/HomePropertyView.kt
+++ app/src/main/java/abrayudhistira/cobafinal/ui/property/HomePropertyView.kt
@@ -1,24 +1,30 @@
 package abrayudhistira.cobafinal.ui.property
 
+import abrayudhistira.cobafinal.R
 import abrayudhistira.cobafinal.model.Properti
 import abrayudhistira.cobafinal.ui.navigasi.DestinasiNavigasi
 import android.content.Context
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Divider
@@ -29,12 +35,42 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.recyclerview.widget.RecyclerView
 
 object DestinasiHome: DestinasiNavigasi {
     override val route = "home"
     override val titleRes = "Home Property"
+}
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Image(
+            modifier = Modifier.size(200.dp),
+            painter = painterResource(R.drawable.no_wifi),
+            contentDescription = stringResource(R.string.loading)
+        )
+    }
+}
+
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Image(painter = painterResource(id = R.drawable.no_wifi), contentDescription = "")
+            Text(text = stringResource(R.string.loading_failed), style = MaterialTheme.typography.bodyMedium, modifier = Modifier.padding(16.dp))
+            Button(onClick = retryAction) {
+                Text(stringResource(R.string.retry))
+            }
+        }
+    }
 }
 
 @Composable
