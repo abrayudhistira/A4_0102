532fe18faab6d245a99f4c63dee79aa4ecf9c39f
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-01-23 08:46:52 +0700

    Code DestinasiNavigasi beserta route nya

--- app/src/main/java/abrayudhistira/cobafinal/ui/navigasi/DestinasiNavigasi.kt
+++ app/src/main/java/abrayudhistira/cobafinal/ui/navigasi/DestinasiNavigasi.kt
@@ -1,2 +1,10 @@
 package abrayudhistira.cobafinal.ui.navigasi
 
+interface DestinasiNavigasi{
+    val route : String
+    val titleRes : String
+}
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Property"
+}
