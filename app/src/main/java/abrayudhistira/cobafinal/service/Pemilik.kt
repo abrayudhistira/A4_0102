2af7ed135c87250512bf25cbd56111e7f6f2da33
abrayudhistira <127074414+abrayudhistira@users.noreply.github.com>
2025-01-24 03:09:30 +0700

    Membuat Service Pemilik

---
+++ app/src/main/java/abrayudhistira/cobafinal/service/Pemilik.kt
@@ -0,0 +1,33 @@
+package abrayudhistira.cobafinal.service
+
+import abrayudhistira.cobafinal.model.Properti
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface Pemilik {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json",
+    )
+
+    @GET("pemilik/listpemilik")
+    suspend fun getPemilik(): List<Pemilik>
+
+    @GET("pemilik/{id_pemilik}")
+    suspend fun getbyidPemilik(@Path("id_pemilik")idPemilik: String): Pemilik
+
+    @POST("pemilik/add")
+    suspend fun insertPemilik(@Body pemilik: Pemilik)
+
+    @PUT("pemilik/edit/{id_pemilik}")
+    suspend fun editPemilik(@Path("id_pemilik")idPemilik: String, @Body pemilik: Pemilik)
+
+    @DELETE("pemilik/delete/{id_pemilik}")
+    suspend fun deletePemilik(@Path("id_pemilik")idPemilik: String): Response<Void>
+}
